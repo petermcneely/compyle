747d12cd8bf230cb7a417d6ff1d966a8234af4ac
Zachary Hine <zacharyhine@gmail.com>
2022-10-14 22:06:08 -0400

    Update test.ml
    Co-authored-by: Peter McNeely <15706233+petermcneely@users.noreply.github.com>

--- test.ml
+++ test.ml
@@ -14,7 +14,7 @@ let pass_test () =
   passed_tests := (!passed_tests + 1);
   print_endline "\tYAY" in
 
-let run_test ?(debug: bool = true) (test_case: string) (input: string) (output: string list): unit =
+let run_test ?(debug: bool = false) (test_case: string) (input: string) (output: string list): unit =
   print_endline (test_case ^ ":");
   count_tests := (!count_tests + 1);
   let parsed_program = parse_program input in
