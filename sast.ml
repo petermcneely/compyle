8a3ba7baec6abcce1dc80c350b49b0f80472023f
Jacky Wong <hw2813@columbia.edu>
2022-11-19 12:25:43 -0500

    semant work in progress

--- sast.ml
+++ sast.ml
@@ -3,8 +3,9 @@
 open Ast
 
 type sexpr = typ * sx
+
 and sx =
-    SIntLit of int
+  | SIntLit of int
   | SFloatLit of float
   | SStringLit of string
   | SBoolLit of bool
@@ -19,8 +20,8 @@ and sx =
   | SNotIn of sexpr * sexpr
   | SCall of string * sexpr list
 
-type sstmt = 
-    SBreak
+type sstmt =
+  | SBreak
   | SContinue
   | SExpr of sexpr
   | SFunction of string * decl list * typ * sstmt list
@@ -34,53 +35,65 @@ type sstmt =
 type sprogram = sstmt list
 
 (* Pretty-printing functions *)
-let rec string_of_sexpr (t, e) = 
-  "(" ^ string_of_typ t ^ " : " ^ (match e with
-        SIntLit i -> string_of_int i
-      | SFloatLit f -> string_of_float f
-      | SStringLit s -> s
-      | SBoolLit b -> string_of_bool b
-      | SId s -> s
-      | SArrayLit l -> "[" ^ string_of_sexprs l ^ "]"
-      | STupleLit l -> "(" ^ string_of_sexprs l ^ ")"
-      | SBinop (e1, op, e2) -> string_of_sexpr e1 ^ " " ^ string_of_bin_op op ^ " " ^ string_of_sexpr e2
-      | SAsn (v, e) -> v ^ " = " ^ string_of_sexpr e
-      | SAugAsn (v, op, e) -> v ^ " " ^ string_of_aug_op op ^ " " ^ string_of_sexpr e
-      | SNot e -> "not " ^ string_of_sexpr e
-      | SIn (e1, e2) -> string_of_sexpr e1 ^ " in " ^ string_of_sexpr e2
-      | SNotIn (e1, e2) -> string_of_sexpr e1 ^ " not in " ^ string_of_sexpr e2
-      | SCall (fn, args) -> fn ^ "(" ^ string_of_sexprs args ^ ")"
-      ) ^ ")"
+let rec string_of_sexpr (t, e) =
+  "(" ^ string_of_typ t ^ " : "
+  ^ (match e with
+    | SIntLit i -> string_of_int i
+    | SFloatLit f -> string_of_float f
+    | SStringLit s -> s
+    | SBoolLit b -> string_of_bool b
+    | SId s -> s
+    | SArrayLit l -> "[" ^ string_of_sexprs l ^ "]"
+    | STupleLit l -> "(" ^ string_of_sexprs l ^ ")"
+    | SBinop (e1, op, e2) ->
+        string_of_sexpr e1 ^ " " ^ string_of_bin_op op ^ " "
+        ^ string_of_sexpr e2
+    | SAsn (v, e) -> v ^ " = " ^ string_of_sexpr e
+    | SAugAsn (v, op, e) ->
+        v ^ " " ^ string_of_aug_op op ^ " " ^ string_of_sexpr e
+    | SNot e -> "not " ^ string_of_sexpr e
+    | SIn (e1, e2) -> string_of_sexpr e1 ^ " in " ^ string_of_sexpr e2
+    | SNotIn (e1, e2) -> string_of_sexpr e1 ^ " not in " ^ string_of_sexpr e2
+    | SCall (fn, args) -> fn ^ "(" ^ string_of_sexprs args ^ ")")
+  ^ ")"
 
-and string_of_sexprs l = 
+and string_of_sexprs l =
   if List.length l = 1 then string_of_sexpr (List.hd l)
-  else if List.length l > 1 then string_of_sexpr (List.hd l) ^ ", " ^ string_of_sexprs (List.tl l)
+  else if List.length l > 1 then
+    string_of_sexpr (List.hd l) ^ ", " ^ string_of_sexprs (List.tl l)
   else ""
 
-let rec string_of_sstmt = function 
-    SBreak -> "break\n"
-    | SContinue -> "continue\n"
-    | SExpr e -> string_of_sexpr e ^ "\n"
-    | SFunction (fname, formals, return_type, block) -> "def " ^ fname ^ "(" ^ string_of_formals formals formals ^ ") -> " ^ string_of_typ return_type ^ ":" ^ string_of_sblock block
-    | SReturn e -> "return " ^ string_of_sexpr e ^ "\n"
-    | SIf (e, if_block, elif_blocks) -> "if " ^ string_of_sexpr e ^ ":" ^ string_of_sblock if_block ^ string_of_selif_blocks elif_blocks
-    | SWhile (e, block) -> "while " ^ string_of_sexpr e ^ ":" ^ string_of_sblock block
-    | SFor (v, e, b) -> "for " ^ v ^ " in " ^ string_of_sexpr e ^ ":" ^ string_of_sblock b
-    | SPrint e -> "print(" ^ string_of_sexpr e ^ ")\n"
-    | SDecl (id, typ) -> string_of_decl (id, typ) ^ "\n"
+let rec string_of_sstmt = function
+  | SBreak -> "break\n"
+  | SContinue -> "continue\n"
+  | SExpr e -> string_of_sexpr e ^ "\n"
+  | SFunction (fname, formals, return_type, block) ->
+      "def " ^ fname ^ "("
+      ^ string_of_formals formals formals
+      ^ ") -> " ^ string_of_typ return_type ^ ":" ^ string_of_sblock block
+  | SReturn e -> "return " ^ string_of_sexpr e ^ "\n"
+  | SIf (e, if_block, elif_blocks) ->
+      "if " ^ string_of_sexpr e ^ ":" ^ string_of_sblock if_block
+      ^ string_of_selif_blocks elif_blocks
+  | SWhile (e, block) ->
+      "while " ^ string_of_sexpr e ^ ":" ^ string_of_sblock block
+  | SFor (v, e, b) ->
+      "for " ^ v ^ " in " ^ string_of_sexpr e ^ ":" ^ string_of_sblock b
+  | SPrint e -> "print(" ^ string_of_sexpr e ^ ")\n"
+  | SDecl (id, typ) -> string_of_decl (id, typ) ^ "\n"
 
 and string_of_selif_blocks elif_blocks =
-  String.concat "\n" (List.map (fun sstmt -> "else:" ^ string_of_sblock [sstmt]) elif_blocks)
+  String.concat "\n"
+    (List.map (fun sstmt -> "else:" ^ string_of_sblock [ sstmt ]) elif_blocks)
 
-and string_of_sblock (l: sstmt list): string =
-  if List.length l > 0 then "\n" ^ (String.concat "" (List.map string_of_sstmt l))
+and string_of_sblock (l : sstmt list) : string =
+  if List.length l > 0 then "\n" ^ String.concat "" (List.map string_of_sstmt l)
   else ""
 
-and string_of_sline (l: sstmt): string =
-  string_of_sstmt l
+and string_of_sline (l : sstmt) : string = string_of_sstmt l
 
-let string_of_sprogram (p: sstmt list): string =
-  (String.concat "" (List.map string_of_sstmt p))
+let string_of_sprogram (p : sstmt list) : string =
+  String.concat "" (List.map string_of_sstmt p)
 
-let pretty_string_of_sprogram (p: sstmt list): string =
+let pretty_string_of_sprogram (p : sstmt list) : string =
   "\n\nParsed Program: \n\n" ^ string_of_sprogram p ^ "<EOF>\n"
---
+++ semant.ml
@@ -0,0 +1,164 @@
+open Ast
+open Sast
+module StringMap = Map.Make (String)
+
+(* Added defs below from ast.ml for VSCode syntax checking *)
+type typ = Int | Bool | Float | String | Tuple | Array of typ
+type aug_op = AAAdd | AASub | AAMult | AADiv | AAMod | AAExp | AAFDiv
+
+type bin_op =
+  | Add
+  | Sub
+  | Mult
+  | Div
+  | Mod
+  | Exp
+  | FDiv
+  | And
+  | Or
+  | Eq
+  | Neq
+  | Gt
+  | Lt
+  | Geq
+  | Leq
+
+type expr =
+  | IntLit of int
+  | FloatLit of float
+  | StringLit of string
+  | BoolLit of bool
+  | ArrayLit of expr list
+  | TupleLit of expr list
+  | Binop of expr * bin_op * expr
+  | Id of string
+  | Asn of string * expr
+  | AugAsn of string * aug_op * expr
+  | Not of expr
+  | In of expr * expr
+  | NotIn of expr * expr
+  | Call of string * expr list
+
+type decl = string * typ
+
+type stmt =
+  | Break
+  | Continue
+  | Expr of expr
+  | Function of string * decl list * typ * stmt list
+  | Return of expr
+  | If of expr * stmt list * stmt list
+  | While of expr * stmt list
+  | For of string * expr * stmt list
+  | Print of expr
+  | Decl of string * typ
+
+type program = stmt list
+(* Added defs above from ast.ml for VSCode syntax checking *)
+
+(* Added defs below from sast.ml for VSCode syntax checking *)
+type sexpr = typ * sx
+
+and sx =
+  | SIntLit of int
+  | SFloatLit of float
+  | SStringLit of string
+  | SBoolLit of bool
+  | SArrayLit of sexpr list
+  | STupleLit of sexpr list
+  | SBinop of sexpr * bin_op * sexpr
+  | SId of string
+  | SAsn of string * sexpr
+  | SAugAsn of string * aug_op * sexpr
+  | SNot of sexpr
+  | SIn of sexpr * sexpr
+  | SNotIn of sexpr * sexpr
+  | SCall of string * sexpr list
+
+type sstmt =
+  | SBreak
+  | SContinue
+  | SExpr of sexpr
+  | SFunction of string * decl list * typ * sstmt list
+  | SReturn of sexpr
+  | SIf of sexpr * sstmt list * sstmt list
+  | SWhile of sexpr * sstmt list
+  | SFor of string * sexpr * sstmt list
+  | SPrint of sexpr
+  | SDecl of string * typ
+
+type sprogram = sstmt list
+(* Added defs above from sast.ml for VSCode syntax checking *)
+
+let rec check (program : program) : sprogram =
+  (* in-place mutation *)
+  let var_decls = Hashtbl.create 100 in
+  let find_var_type (id : string) : typ =
+    try Hashtbl.find var_decls id
+    with Not_found -> raise (Failure ("Unbound variable " ^ id))
+  in
+  let rec check_expr (e : expr) : sexpr =
+    match e with
+    | IntLit i -> (Int, SIntLit i)
+    | FloatLit f -> (Float, SFloatLit f)
+    | StringLit s -> (String, SStringLit s)
+    | BoolLit b -> (Bool, SBoolLit b)
+    | ArrayLit a -> raise (Failure "Not implemented")
+    | TupleLit t -> (Tuple, STupleLit (List.map check_expr t))
+    | Binop (e1, bin_op, e2) ->
+        let t1, e1' = check_expr e1 and t2, e2' = check_expr e2 in
+        if t1 = t2 then
+          let t =
+            match bin_op with
+            | (Add | Sub | Mult | FDiv | Mod | Exp) when t1 = Int -> Int
+            | (Add | Sub | Mult | Div | Mod | Exp) when t1 = Float -> Float
+            | (And | Or) when t1 = Bool -> Bool
+            | (Eq | Neq) when t1 = Bool -> Bool
+            | (Gt | Lt | Geq | Leq) when t1 = Float -> Bool
+            | (Gt | Lt | Geq | Leq) when t1 = Int -> Bool
+            | _ -> raise (Failure "Incompatible operands")
+          in
+          (t, SBinop ((t1, e1'), bin_op, (t2, e2')))
+        else raise (Failure "Incompatible operands")
+    | Id var -> (find_var_type var, SId var)
+    | Asn (var, e) ->
+        let t = find_var_type var and t', e' = check_expr e in
+        if t = t' then (t, SAsn (var, (t', e')))
+        else raise (Failure ("Unbound variable " ^ var))
+    | AugAsn (var, aug_op, e) ->
+        let t1 = find_var_type var and t2, e' = check_expr e in
+        if t1 = t2 then (t1, SAugAsn (var, aug_op, (t2, e')))
+        else raise (Failure ("Unbound variable " ^ var))
+    | Not e ->
+        let t, e' = check_expr e in
+        if t = Bool then (t, SNot (t, e'))
+        else raise (Failure "Expect boolean expression")
+    | NotIn (e1, e2) -> raise (Failure "Not implemented")
+    | Call (id, f_block) -> raise (Failure "Not implemented")
+    | _ -> raise (Failure "Semantically invalid expression")
+  in
+  let check_stmt (stmt : stmt) : sstmt =
+    match stmt with
+    | Break -> SBreak
+    | Continue -> SContinue
+    | Expr e -> SExpr (check_expr e)
+    | Function (fname, args, t, f_block) -> raise (Failure "Not implemented")
+    | Return e -> SReturn (check_expr e)
+    | If (e, if_block, else_block) ->
+        let t, e' = check_expr e
+        and s_if_block = check if_block
+        and s_else_block = check else_block in
+        if t = Bool then SIf ((t, e'), s_if_block, s_else_block)
+        else raise (Failure "Expect boolean expression")
+    | While (e, while_block) ->
+        let t, e' = check_expr e and s_while_block = check while_block in
+        if t = Bool then SWhile ((t, e'), s_while_block)
+        else raise (Failure "Expect boolean expression")
+    | For (id, e, for_block) -> raise (Failure "Not implemented")
+    | Print e -> SPrint (check_expr e)
+    | Decl (id, t) ->
+        Hashtbl.add var_decls id t;
+        (* decls can be overwritten and created at any point *)
+        SDecl (id, t)
+  in
+  List.map check_stmt program
